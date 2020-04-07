6e9f647fa5fd77b480cb4c01c24ea3cd68f9a183
elmotan95 <elmo@momokun.id>
2020-04-07 17:09:47 +0700

    chore: move to dependencies

--- src/TabViewPagerAndroid.js
+++ src/TabViewPagerAndroid.js
@@ -1,7 +1,8 @@
 /* @flow */
 
 import * as React from 'react';
-import { View, ViewPagerAndroid, StyleSheet, I18nManager } from 'react-native';
+import { View, StyleSheet, I18nManager } from 'react-native';
+import ViewPager from '@react-native-community/viewpager';
 import { PagerRendererPropType } from './TabViewPropTypes';
 import type { PagerRendererProps } from './TabViewTypeDefinitions';
 
@@ -42,7 +43,7 @@ export default class TabViewPagerAndroid<T: *> extends React.Component<
   }
 
   _pageChangeCallabck: any;
-  _viewPager: ?ViewPagerAndroid;
+  _viewPager: ?ViewPager;
   _isIdle: boolean = true;
   _currentIndex = 0;
 
@@ -103,7 +104,7 @@ export default class TabViewPagerAndroid<T: *> extends React.Component<
     this._currentIndex = index;
   };
 
-  _setRef = (el: ?ViewPagerAndroid) => (this._viewPager = el);
+  _setRef = (el: ?ViewPager) => (this._viewPager = el);
 
   render() {
     const { children, navigationState, swipeEnabled } = this.props;
@@ -124,7 +125,7 @@ export default class TabViewPagerAndroid<T: *> extends React.Component<
     const initialPage = this._getPageIndex(navigationState.index);
 
     return (
-      <ViewPagerAndroid
+      <ViewPager
         key={navigationState.routes.length}
         keyboardDismissMode="on-drag"
         initialPage={initialPage}
@@ -136,7 +137,7 @@ export default class TabViewPagerAndroid<T: *> extends React.Component<
         ref={this._setRef}
       >
         {content}
-      </ViewPagerAndroid>
+      </ViewPager>
     );
   }
 }
